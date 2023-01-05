d663519c635515f9d7681f4af2c789b141df01c4
harsha-ys <hyoshith@gmail.com>
2023-01-06 01:54:43 +0530

    arrays

---
+++ Section 10 intermediate javascript/arrayexample.js
@@ -0,0 +1,4 @@
+var names = ["yoshith", "sri", "harshana"];
+var nametobechecked = prompt("give me your name ");
+if(names.includes(nametobechecked)) alert("you are already there")
+else alert("you are not there")
