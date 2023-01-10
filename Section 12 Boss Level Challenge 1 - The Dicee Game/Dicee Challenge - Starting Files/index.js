ffa8877bd406b7860d6f83f1d400540632bc8bed
harsha-ys <hyoshith@gmail.com>
2023-01-10 06:20:43 +0530

    correction in paths of the images

--- Section 12 Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/index.js
+++ Section 12 Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/index.js
@@ -1,4 +1,4 @@
 var randomNumber1 = Math.round(Math.random()*6);
 
-document.querySelector('img1').setAttribute('src', 'dice'+ randomNumber1 + '.png');
-document.querySelector('img2').setAttribute('src', 'dice'+ randomNumber1 + '.png');
+document.querySelector('img1').setAttribute('src', 'images/' + 'dice'+ randomNumber1 + '.png');
+document.querySelector('img2').setAttribute('src', 'images/' + 'dice'+ randomNumber1 + '.png');
