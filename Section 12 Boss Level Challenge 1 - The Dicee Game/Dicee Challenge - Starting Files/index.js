6d6157c4864f5c46ca6f9e3174aa050ea55821d8
harsha-ys <hyoshith@gmail.com>
2023-01-10 11:37:10 +0530

    the dicee game which display the winner according to the rendom number generated

--- Section 12 Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/index.js
+++ Section 12 Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/index.js
@@ -1,4 +1,18 @@
-var randomNumber1 = Math.round(Math.random()*6);
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
 
-document.querySelector('img1').setAttribute('src', 'images/' + 'dice'+ randomNumber1 + '.png');
-document.querySelector('img2').setAttribute('src', 'images/' + 'dice'+ randomNumber1 + '.png');
+var path1 = "images/" + "dice"+ randomNumber1 + ".png";
+var path2 = "images/" + "dice"+ randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", path1);
+document.querySelector(".img2").setAttribute("src", path2);
+
+if (randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player1 Wins";
+}
+else if (randomNumber2> randomNumber1)
+{
+    document.querySelector("h1").innerHTML="Player2 Wins";
+}
+else document.querySelector("h1").innerHTML = "Draw";
